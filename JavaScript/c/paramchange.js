49e6e84769de12eb337d1248ed524b33a590e21f
194013oono <194013@ocsjoho.onmicrosoft.com>
2022-11-22 12:18:07 +0900

    11/22　パラメータ変動地入力完了

--- JavaScript/c/paramchange.js
+++ JavaScript/c/paramchange.js
@@ -25,22 +25,31 @@ function task_failed(task_type){
     switch (task_type){
         case "A":
             paramctr([0,-10],[1,-10],[2,-10],[3,-10],[4,-10],[5,-10],[6,-10],[7,-10],[8,-10],[9,-10],[10,-10]);
+            break;
         case "A1":
             paramctr([0,-5],[1,-5],[2,-5],[3,-5],[4,-5],[5,-5],[6,-5],[7,-5],[8,-5],[9,-5],[10,-5]);
+            break;
         case "A2":
             paramctr([0,-5],[1,-5],[2,-5],[3,-5],[4,-5],[5,-5],[6,-5],[7,-5],[8,-5],[9,-5],[10,-5]);
+            break;
         case "A21":
             paramctr([0,-3],[1,-3],[2,-3],[3,-3],[4,-3],[5,-3],[6,-3],[7,-3],[8,-3],[9,-3],[10,-3]);
+            break;
         case "B":
             paramctr([0,-10],[1,-10],[2,-10],[3,-10],[4,-10],[5,-10],[6,-10],[7,-10],[8,-10],[9,-10],[10,-10]);
+            break;
         case "B1":
             paramctr([0,-5],[1,-5],[2,-5],[3,-5],[4,-5],[5,-5],[6,-5],[7,-5],[8,-5],[9,-5],[10,-5]);
+            break;
         case "B2":
             paramctr([0,-5],[1,-5],[2,-5],[3,-5],[4,-5],[5,-5],[6,-5],[7,-5],[8,-5],[9,-5],[10,-5]);
+            break;
         case "C":
             paramctr([0,-10],[1,-10],[2,-10],[3,-10],[4,-10],[5,-10],[6,-10],[7,-10],[8,-10],[9,-10],[10,-10]);
+            break;
         case "X":
             paramctr([0,-3],[1,-3],[2,-3],[3,-3],[4,-3],[5,-3],[6,-3],[7,-3],[8,-3],[9,-3],[10,-3]);
+            break;
         }
 }
 
@@ -80,12 +89,16 @@ function A_spam_recovery(difficulty){
     switch (difficulty){
         case 1:
             paramctr([[1,-1],[2,-1],[6,-1],[7,-1],[8,-1]]);
+            break;
         case 2:
             paramctr([[1,-2],[2,-2],[6,-2],[7,-2],[8,-2]]);
+            break;
         case 3:
             paramctr([[1,-3],[2,-3],[6,-3],[7,-3],[8,-3]]);
+            break;
         case 4:
             paramctr([[1,-4],[2,-4],[6,-4],[7,-4],[8,-4]]);
+            break;
     }
 }
 
@@ -94,12 +107,16 @@ function A_spam_trash(difficulty){
     switch (difficulty){
         case 1:
             paramctr([[1,2],[2,2],[6,2],[7,2],[8,2]]);
+            break;
         case 2:
             paramctr([[1,3],[2,3],[6,3],[7,3],[8,3]]);
+            break;
         case 3:
             paramctr([[1,4],[2,4],[6,4],[7,4],[8,4]]);
+            break;
         case 4:
             paramctr([[1,5],[2,5],[6,5],[7,5],[8,5]]);
+            break;
     }
 }
 
@@ -133,28 +150,25 @@ function A1_res_mistake(){
 
 /*
 A-2
+（Xと共通）
 */
-//パスワードを付ける資料を間違える
+//パスワードを付ける資料を間違える（独立）
 function A2_doc_mistake(){
     paramctr([[0,-1],[1,-1],[3,-1]]);
 }
 
 //使用文字が1、2種類だけのPWで完了
-function A2_pass_type(pass){
-    //パスワードの文字種類を計算
-    //let type;
+function A2_pass_type(type){
     if(type==1)         paramctr([[0,-10],[3,-10],[6,-10],[7,-10],[10,-10]]);
-    else if(type>=2){
-        if(type==2) paramctr([[0,-5],[3,-5],[6,-5],[7,-5],[10,-5]]);
-        A2_pass_kigou(pass);
-    }
+    else if(type==2)    paramctr([[0,-5],[3,-5],[6,-5],[7,-5],[10,-5]]);
 }
 
 //記号を使用
-function A2_pass_kigou(pass){
-    if(pass){           //大文字、小文字、数字、記号を全部
+//記号使用フラグ（仮）を使用
+function A2_pass_kigou(hoge){
+    if(hoge){           //大文字、小文字、数字、記号を全部
         paramctr([[0,1],[3,1],[5,1],[6,1],[7,1],[10,1]]);
-    }else if(pass){     //記号のどれかを使用
+    }else if(hoge){     //記号のどれかを使用
         paramctr([[0,1],[3,1],[6,1],[7,1],[10,1]]);
     }
 }
@@ -179,17 +193,35 @@ function A2_1min_finish(){
 /*
 A-2-1
 */
-//aiueo株式会社が送信先以外に入っている
+//aiupro株式会社が送信先以外に入っている
+function A21_to_except_aiupro(){
+    paramctr([[0,-8],[1,-8],[2,-8],[3,-8],[5,-5]]);
+}
 
-//aiueo株式会社が送信先に入っていない
+//aiupro株式会社が送信先に入っていない
+function A21_to_not_aiupro(){
+    paramctr([[0,-3],[1,-8],[2,-1],[3,-5],[4,-8],[5,-1]]);
+}
 
 //stockmoney株式会社がBCC以外に入っている
+function A21_BCC_except_stockmoney(){
+    paramctr([[0,-8],[1,-8],[2,-8],[3,-8],[5,-5],[7,-10]]);
+}
 
 //stockmoney株式会社がBCCに入っていない
+function A21_BCC_not_stockmoney(){
+    paramctr([[0,-8],[1,-8],[2,-8],[3,-8],[4,-8],[5,-7]]);
+}
 
 //BCCに意図しないアドレスが入っている
+function A21_BCC_unintentional(){
+    paramctr([[0,-8],[1,-8],[2,-8],[3,-8],[7,-10],[9,-10]]);
+}
 
 //CCに意図しないアドレスが入っている
+function A21_CC_unintentional(){
+    paramctr([[0,-8],[1,-8],[2,-8],[3,-8],[7,-10],[9,-10]]);
+}
 
 //projectXXの資料を編集
 function A21_projectXX_edit(){
@@ -214,14 +246,19 @@ function B1_rate_input(rate){
     switch(rate){
         case 1:
             paramctr([[1,-10],[3,-10],[5,-10],[7,-10],[8,-10]]);
+            break;
         case 2:
             paramctr([[1,-7],[3,-7],[5,-7],[7,-7],[8,-7]]);
+            break;
         case 3:
             paramctr([[1,-4],[3,-4],[5,-4],[7,-4],[8,-4]]);
+            break;
         case 4:
             paramctr([[1,-2],[3,-2],[5,-2],[7,-2],[8,-2]]);
+            break;
         case 5:
             paramctr([[1,-1],[3,-1],[5,-1],[7,-1],[8,-1]]);
+            break;
     }
 }
 
@@ -263,8 +300,10 @@ function B2_ivent_input(ivent){
     switch(ivent){
         case 1:
             paramctr([[1,-5],[3,-5],[5,-5],[7,-5],[8,-5]]);
+            break;
         case 2:
             paramctr([[1,-2],[3,-2],[5,-2],[7,-2],[8,-2]]);
+            break;
     }
 }
 
@@ -323,4 +362,122 @@ function C_2min_finish(){
 
 /*
 X
+A-2とほぼ共通
 */
+
+/*
+Z
+*/
+//悪いサイトにアクセス
+function Z_access_bad_page(){
+    paramctr([[0,-5],[2,-6],[3,-1],[6,-1],[7,-1][8,-4],[9,-5],[11,2]]);
+}
+
+//一度に2つ以上の完了報告をする
+function Z_task_report(num=0){
+    switch(num){
+        case 0:
+        case 1:
+            break;
+        case 2:
+            paramctr([[4,-2],[5,-2]]);
+            break;
+        case 3:
+            paramctr([[4,-6],[5,-2]]);
+            break;
+        case 4:
+            paramctr([[4,-8],[5,-2]]);
+            break;
+        default:
+            paramctr([[4,-10],[5,-2]]);
+            break;
+    }
+}
+
+//業務に関係ない広告メールのリンクをクリック
+function Z_spam_mail_click(){
+    paramctr([[0,-3],[2,-8],[6,-5],[9,-8],[11,8]]);
+}
+
+//制限時間内に終わらない
+function Z_time_over(){
+    paramctr([[5,-1]]);
+}
+
+//タスクが終わっていない状況でシャットダウン、再起動、更新
+function Z_unfinished_shutdown(){
+    paramctr([[5,-10],[11,10]]);
+}
+
+//不正なリンク、ファイルを開く
+function Z_NG_link_click(){
+    paramctr([[2,-5],[7,-5],[8,-5],[10,-5],[11,6]]);
+}
+
+//タスクと関係ない時検索画面で株価とニュース以外を調べる
+function Z_task_disrelation_search(){
+    paramctr([[1,-6],[5,-6],[6,-5],[11,6]]);
+}
+
+//タスクと関係ない時検索画面で株価とニュースを調べる
+function Z_task_disrelation_search_news(){
+    paramctr([[1,-2],[5,-2],[11,3]]);
+}
+
+//良いメールを消す
+function Z_OK_mail_trash(){
+    paramctr([[1,-2],[2,-2],[3,-2],[11,2]]);
+}
+
+//404が出る
+function Z_display_404(){
+    paramctr([[0,-2],[2,-2],[5,-4],[6,-3],[7,-2][8,-2],[11,5]]);
+}
+
+//タスクに関係ないタイミングでパスワード編集
+function Z_pass_edit(){
+    paramctr([[0,-5],[11,5]]);
+}
+
+//タスクの指示に関係なくメール送信
+function Z_mail_disrelation_send(){
+    paramctr([[0,-8],[1,-8],[5,-10],[11,10]]);
+}
+
+//資料に不要な書き込み
+function Z_doc_edit(){
+    paramctr([[1,-1],[3,-1],[5,-1],[11,1]]);
+}
+
+//メールをゴミ箱から戻した
+function Z_mail_recovery(){
+    paramctr([[0,-1],[1,-1],[2,-1],[7,-1],[11,1]]);
+}
+
+//タスクで使わない編集可能ファイルの内容を勝手に消す
+function Z_doc_delete(){
+    paramctr([[5,-4],[6,-1],[7,-4],[9,-4],[11,3]]);
+}
+
+//タスクがすべて完了したうえでOSを更新
+function Z_task_finished_update(){
+    paramctr([[0,2],[3,2],[5,5],[6,5],[7,2]]);
+}
+
+//難易度〇の迷惑メールを1個消す（Z）
+function Z_spam_trash(difficulty){
+    switch (difficulty){
+        case 1:
+            paramctr([[1,1],[2,1],[6,2],[7,2],[8,2]]);
+            break;
+        case 2:
+            paramctr([[1,2],[2,2],[6,3],[7,3],[8,3]]);
+            break;
+        case 3:
+            paramctr([[1,3],[2,3],[6,4],[7,4],[8,4]]);
+            break;
+        case 4:
+            paramctr([[1,4],[2,4],[6,5],[7,5],[8,5]]);
+            break;
+    }
+}
