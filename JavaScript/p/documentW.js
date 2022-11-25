84939ccec1150f7d8175688a7fc7d355c6c670a1
194013oono <194013@ocsjoho.onmicrosoft.com>
2022-11-25 12:29:38 +0900

    11/25ファイル編集処理を実装中

--- JavaScript/p/documentW.js
+++ JavaScript/p/documentW.js
@@ -33,3 +33,55 @@ window.addEventListener('beforeunload', function (e) {
     // Chrome では returnValue を設定する必要がある
     e.returnValue = '';
   });
+
+//保存内容の判定
+function text_judge(){
+    //ファイル名を取得
+    url = new URL(window.location.href);
+    params = url.searchParams;
+    file_name = params.get("file");
+
+    //テキストを取得
+    text = localStorage.getItem("file_"+ file_name);
+
+    if(file_name == "10_Company")           judge_10_Company(text);
+    else if(file_name== "ExchangeRate")     judge_ExchangeRate(text);
+}
+
+function judge_10_Company(text){
+    
+}
+
+function judge_ExchangeRate(text){
+
+    const keyword = {
+        "日経平均":"25,937.21",
+        "米ドル/円":"144.72",
+        "TOPIX":"1,835.94",
+        "NYダウ":"28,725.51",
+        "上海総合":"3,024.39",
+        "ユーロ/円":"141.84"
+    };
+
+
+    let rate_num = 0;
+    //正しく記入されているレート数を検出
+    for(i in keyword){
+        if(text.indexOf(i) != -1){
+            if(text.indexOf(keyword[i]) != -1){
+                console.log(i+keyword[i]);
+                rate_num++;
+            }else{
+                //記入レートの数値に間違いがある
+                //
+            }
+        }
+
+        //正しく記入されているレート数に応じてパラメータ変動
+        switch(rate_num){
+            case 0:
+                //
+        }
+    }
+
+}
